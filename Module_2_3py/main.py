c9d9b73bfa6edfeb5be785777694cb16a96fa18b
Proscka <proskinaviktoria861@gmail.com>
2024-07-09 14:16:48 +0300

    Add files via upload

---
+++ Module_2_3py/main.py
@@ -0,0 +1,52 @@
+my_list=[42,69,322,13,0,99,-5,9,8,7,-6,5]
+print(my_list)
+i = 0
+while i < len(my_list):
+    for i in my_list[0:4]:
+        print (i)
+    for i in my_list[5:6]:
+        print(i)
+    break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
